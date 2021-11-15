d82887752f0d30f01ee5e98f30df48bc2deba009
Ryanheo15 <Ryanheo2001@gmail.com>
2021-11-14 21:24:12 -0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 8 - Starter
+1. You would fit your automated tests within a github action that runs whenever code is pushed
+2. No 
+3. No I would not use unit tests to test messaging functionality due to messaging being a dynamic funcitonality with components moving back and forth requiring multiple components to interact with one another. This would make more sense for integration testing/E2E testing. 
+4. Yes, because that is a single testable item and doesnt require interaction between other components on the screen
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,38 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('Valid phone number test', () => {
+  expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+  expect(functions.isPhoneNumber('858-111-7893')).toBe(true);
+  expect(functions.isPhoneNumber('1')).toBe(false);
+  expect(functions.isPhoneNumber('111')).toBe(false);
+});
+
+test('Valid email test', () => {
+  expect(functions.isEmail('hello@gmail.com')).toBe(true);
+  expect(functions.isEmail('hello@yahoo.com')).toBe(true);
+  expect(functions.isEmail('hello')).toBe(false);
+  expect(functions.isEmail('hello@')).toBe(false);
+});
+
+test('Password strength test', () => {
+  expect(functions.isStrongPassword('A893J9')).toBe(true);
+  expect(functions.isStrongPassword('KO839kds9')).toBe(true);
+  expect(functions.isStrongPassword('1')).toBe(false);
+  expect(functions.isStrongPassword('A')).toBe(false);
+});
+
+test('Data format test', () => {
+  expect(functions.isDate('11/14/2021')).toBe(true);
+  expect(functions.isDate('1/1/2020')).toBe(true);
+  expect(functions.isDate('11/11/11')).toBe(false);
+  expect(functions.isDate('1/1')).toBe(false);
+});
+
+test('Valid hex color test', () => {
+  expect(functions.isHexColor('36D7D7')).toBe(true);
+  expect(functions.isHexColor('FFFFFF')).toBe(true);
+  expect(functions.isHexColor('fjhaskufw')).toBe(false);
+  expect(functions.isHexColor('839*dnfwenwei')).toBe(false);
+});
+
